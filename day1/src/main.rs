6a5f95800854b9c3690ac184344e33c6c6d6322e
Allex Veldman <allexveldman@gmail.com>
2024-12-01 09:37:16 +0100

    Day 1, Puzzle 2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,29 +1,67 @@
 use core::panic;
-use std::{fs, iter::zip};
+use std::{
+    collections::HashMap,
+    fs::File,
+    io::{BufRead, BufReader},
+    iter::zip,
+};
 
 fn read_input() -> (Vec<u32>, Vec<u32>) {
-    let text = fs::read_to_string("input.txt").expect("Could not read the file");
-    let lines = text.lines();
+    let file = File::open("input.txt").expect("Could not read file");
+    let lines = BufReader::new(file).lines();
+
     let mut left = vec![];
     let mut right = vec![];
     for line in lines {
+        let line = line.unwrap();
         let [left_id, right_id] = line.split_whitespace().collect::<Vec<&str>>()[..] else {
             panic!("invalid input")
         };
         left.push(left_id.parse().expect("Parse string to int"));
         right.push(right_id.parse().expect("Parse string to int"));
     }
-    left.sort_unstable();
-    right.sort_unstable();
     (left, right)
 }
 
-fn puzzle_1() {
-    let (left, right) = read_input();
-    let result = zip(left, right).fold(0, |acc, (left, right)| acc + left.abs_diff(right));
+/// Distance between sorted lists
+fn puzzle_1(left: &mut Vec<u32>, right: &mut Vec<u32>) -> u32 {
+    left.sort_unstable();
+    right.sort_unstable();
+    let result = zip(left, right).fold(0, |acc, (left, right)| acc + left.abs_diff(*right));
     println!("{result}");
+    result
+}
+
+/// Similarity
+fn puzzle_2(left: Vec<u32>, right: Vec<u32>) -> u32 {
+    let mut counts = HashMap::new();
+    for value in right {
+        *counts.entry(value).or_insert(0) += 1;
+    }
+    let result = left.iter().fold(0, |acc, value| {
+        acc + value * counts.get(value).unwrap_or(&0)
+    });
+    println!("{result}");
+    result
 }
 
 fn main() {
-    puzzle_1();
+    let (mut left, mut right) = read_input();
+    puzzle_1(&mut left, &mut right);
+    puzzle_2(left, right);
+}
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn puzzle_1() {
+        let result = crate::puzzle_1(&mut vec![3, 4, 2, 1, 3, 3], &mut vec![4, 3, 5, 3, 9, 3]);
+        assert_eq!(result, 11);
+    }
+
+    #[test]
+    fn puzzle_2() {
+        let result = crate::puzzle_2(vec![3, 4, 2, 1, 3, 3], vec![4, 3, 5, 3, 9, 3]);
+        assert_eq!(result, 31);
+    }
 }
